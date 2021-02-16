bb7cb29e1f56d69ee90612665c2a2bb0ccbe4867
dohee479 <dohee3959@gmail.com>
2021-02-16 16:40:06 +0900

    Exam03 추가

--- src/ch02/Exam03.java
+++ src/ch02/Exam03.java
@@ -3,8 +3,21 @@ package ch02;
 public class Exam03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// int 타입 이하의 피연산자의 연산은 결과가 int
+		byte var1 = 10;
+		byte var2 = 20;
+		int var3 = var1 + var2;
+		byte var4 = (byte) (var1 + var2);
+		
+		int var5 = 10;
+		long var6 = 20;
+		long var7 = var5 + var6;
+		
+		int var8 = 10;
+		double var9 = 1.5;
+		double var10 = var8 + var9;
+		System.out.println(var10);
 	}
 
 }
