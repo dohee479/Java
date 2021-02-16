83813832c0ca55dfb453ff959ad1a71ca0879c30
dohee479 <dohee3959@gmail.com>
2021-02-16 16:57:15 +0900

    Exam04 추가

--- src/ch02/Exam04.java
+++ src/ch02/Exam04.java
@@ -3,8 +3,33 @@ package ch02;
 public class Exam04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// 문자열 => 숫자(정수, 실수)
+		
+		// 문자열은 String 타입 변수에 저장한다.
+		String var1 = "10";
+		System.out.println(var1);
+		
+		// 문자열 + 숫자 
+		String var2 = var1 + 5;
+		System.out.println(var2);
+		
+		// 문자열을 숫자로 변경해서 계산
+		int var3 = Integer.parseInt(var1) + 5;
+		System.out.println(var3);
+		
+		// 문자열을 실수로 변경해서 연산
+		String var4 = "10.5";
+		Double var5 = Double.parseDouble(var4) + 2.3;
+		System.out.println(var5);
+		
+		// 숫자 => 문자열
+		int var6 = 3;
+		String var7 = var6 + "0";
+		System.out.println(var7);
+		
+		String var8 = String.valueOf(var6);
+		
 	}
 
 }
