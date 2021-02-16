1459aaf145f5f10732ab66c78a039c06de1c83af
dohee479 <dohee479@naver.com>
2021-02-16 22:58:46 +0900

    git 연습

--- src/ch03/Exam02.java
+++ src/ch03/Exam02.java
@@ -17,6 +17,7 @@ public class Exam02 {
 		boolean var9 = !(var1 == var2);
 		boolean var10 = (var1 == var2 || var1 < var2);
 		boolean var11 = (var1 == var2 && var1 < var2);
+		System.out.println(var11);
 	}
 
 }
