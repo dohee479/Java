367361eb82d89b9304c1785a7a9fc0b2d6280c07
dohee479 <dohee479@naver.com>
2021-02-18 00:48:56 +0900

    homework

--- src/ch05homework/확인문제9.java
+++ src/ch05homework/확인문제9.java
@@ -13,28 +13,45 @@ public class 확인문제9 {
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
 			System.out.println("-------------------------");
 			System.out.print("선택> ");
+		
+		
+		
+			int selectNo = scanner.nextInt();
+			
+			if (selectNo == 1) {
+				System.out.print("학생수> ");
+				int student = scanner.nextInt();
+				studentNum = student;
+				scores = new int[studentNum];
+			} else if (selectNo == 2) {
+				for (int i = 0; i < scores.length; i++) {
+					System.out.print("scores["+i+"]> ");
+					int score = scanner.nextInt();
+					scores[i] = score; 
+				}
+			} else if (selectNo == 3) {
+				for (int i = 0; i < scores.length; i++) {
+					System.out.println("scores["+i+"]:" + scores[i]);
+				}
+			} else if (selectNo == 4) {
+				int maxNum = 0;
+				int sum = 0;
+				for (int i : scores) {
+					if (i > maxNum) {
+						maxNum = i;
+					}
+					sum += i;
+				}
+				System.out.println("최고 점수: " + maxNum);
+				System.out.println("평균 점수: " + (double) sum / scores.length);
+			} else if (selectNo == 5) {
+				run = false;
+			}
+			
+			
 		}
-		
-		
-		int selectNo = scanner.nextInt();
-		
-		if (selectNo == 1) {
-			System.out.print("학생수>");
-			int student = scanner.nextInt();
-			studentNum = student;
-			scores = new int[studentNum];
-		} else if (selectNo == 2) {
-			for (int i = 0; i < scores.length; i++) {
-				scores[i] = scanner.nextInt();
-			}
-		} else if (selectNo == 3) {
-			for (int i = 0; i < scores.length; i++) {
-				System.out.println("scores["+i+"]:" + scores[i]);
-			}
-		} 
+		scanner.close();
 		System.out.println("프로그램 종료");
-		
-
 	}
 
 }
